14123bf66ed2174d9493adf6deecc85e49b508ee
Emma Scott <emmajane1276@gmail.com>
2023-06-13 19:51:57 +0000

    Added class methods to  webhook handler

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -31,5 +31,25 @@ def webhook(request):
     except stripe.error.SignatureVerificationError as e:
         # invalid signature
         return HttpResponse(status=400)
-    except Excption as e:
+    except Exception as e:
         return HttpResponse(content=e, status=400)
+
+    # set up a webhook handler
+    handler = StripeWH_Handler(request)
+
+    # map webhook events to relevant handler functions
+    event_map = {
+        'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
+        'payment_intent.payment_failed': handler.handle_payment_intent_payment_failed,
+    }
+
+    # get the webhook type from Stripe
+    event_type = event['type']
+
+    # If there is a handler for it, get it from the event map
+    # use the generic one by default
+    event_handler = event_map.get(event_type, handler.handle_event)
+
+    # call the event handler with the event
+    response = event_handler(event)
+    return response
