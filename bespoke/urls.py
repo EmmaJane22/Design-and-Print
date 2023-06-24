031be2e2ea85e67cec08b3761685333f0a7554b8
Emma Scott <emmajane1276@gmail.com>
2023-06-24 11:13:33 +0000

    Added bespoke add order functionality

--- bespoke/urls.py
+++ bespoke/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.bespoke_orders, name='bespoke'),
+    path('<int:bespoke_order_id>/', views.bespoke_order_detail, name='bespoke_order_detail'),
+    path('add/', views.add_bespoke_order, name='add_bespoke_order'),
     
 ]
--- bespoke/views.py
+++ bespoke/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
 
-from .models import BespokeOrder
 from .models import UserProfile
+from .models import BespokeOrder
+from .forms import BespokeOrderForm
+
 
 @login_required
 def bespoke_orders(request):
@@ -20,3 +22,30 @@ def bespoke_orders(request):
     }
 
     return render(request, 'bespoke/bespoke_orders.html', context)
+
+
+@login_required
+def bespoke_order_detail(request, bespoke_order_id):
+    return render(request, 'bespoke/bespoke_orders.html', context)
+
+@login_required
+def add_bespoke_order(request):
+    """ Create bespoke order """
+    if request.method == "POST":
+        print('if post = true')
+        form = BespokeOrderForm(request.POST, request.FILES)
+        if form.is_valid():
+            bespoke_order = form.save()
+            messages.success(request, 'Bespoke order added successfully')
+            return redirect(reverse('bespoke')) #, args=[bespoke_order.bespoke_order_number]))
+        else:
+            messages.error(request, 'Failed to add bespoke order.')
+    else:
+        form = BespokeOrderForm()
+
+    template = 'bespoke/add_bespoke_order.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
