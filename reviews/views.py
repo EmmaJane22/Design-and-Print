eede6c4182ace567b15a2bc797ac2194a2725edf
Emma Scott <emmajane1276@gmail.com>
2023-06-24 16:47:34 +0000

    Added reviews page and functionality

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.conf import settings
 
-# Create your views here.
+from .models import Review
+
+def all_reviews(request):
+    """ View to return all reviews in date order """
+
+    reviews = Review.objects.all()
+    reviews.order_by('created_on')
+    
+    context = {
+        'reviews': reviews
+    }
+
+    return render(request, 'reviews/reviews.html', context)
