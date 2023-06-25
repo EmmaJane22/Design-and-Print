e6e98a828c8e5cdd7d027b6e874dfe4eaff0ca94
Emma Scott <emmajane1276@gmail.com>
2023-06-25 16:22:06 +0000

    Added STATIC_ROOT

--- design_print/settings.py
+++ design_print/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+#SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-klt_fy6i!^106cot#n2x=pix42cew7z7m57x-t+hy1&7)epju*')
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -175,6 +176,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
